081f19d09dfd2841e1ebfa6f271a844f418f8a52
Kam <kam@MacBookPro.lan>
2025-09-17 18:11:08 -0400

    Initial commit

---
+++ Server.js
@@ -0,0 +1,34 @@
+const [express, cors, cookieParser, fs, path] = [
+    require("express"),
+    require("cors"),
+    require("cookie-parser"),
+    require("fs"),
+    require("path")
+]
+
+const app = express()
+app.use(cors({ origin: "*" }))
+app.use(express.json())
+
+
+app.get("/", (req, res) => {
+    const ua = req.headers["user-agent"];
+    console.log("User-Agent:", ua);
+
+    const isMobile = /iPhone|iPad|iPod|Android|webOS|BlackBerry|IEMobile|Opera Mini/i.test(ua);
+
+    if (isMobile) {
+        console.log("Visitor is on a mobile device");
+    } else {
+        console.log("Visitor is on desktop/laptop");
+    }
+    
+    res.sendFile(path.join(__dirname, "build", "index.html"))
+    console.log(req.headers["user-agent"])
+})
+
+app.use(express.static(path.join(__dirname, "build"), {index: false}))
+
+app.listen(5500, () => {
+    console.log("listening on port 5500")
+})
